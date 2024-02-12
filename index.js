11401e60301f1fb9de71fddd81edfa54aaa1222f
dheeraj <dheeraj@dheerajs-MacBook-Air.local>
2024-02-12 23:58:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import gemstoneRouter from "./routes/Gemsstone.js";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/gemstone", gemstoneRouter);
+
+app.get("/", (req, res) => {
+  res.send("welcome to the API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
