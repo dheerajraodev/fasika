f326bf560b41b2d32bbb5d96d05998c2189bf023
dheeraj <dheeraj@dheerajs-MacBook-Air.local>
2024-02-13 14:49:47 +0530

    second

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,9 @@
+import mongoose from "mongoose";
 export async function conn() {
-    try {
-        await mongoose.connect(process.env.ATLAS_URI);
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    await mongoose.connect("mongodb://localhost:27018/testdb");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error.message);
+  }
 }
--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import gemstoneRouter from "./routes/Gemsstone.js";
+import { conn } from "./db/conn.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+conn();
 
 app.use(morgan("dev"));
 app.use(express.json());
